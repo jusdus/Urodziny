f49e1816f2fd02c5c7c1eaca93ca7cdf09050225
JusDus <jusdus@wp.pl>
2018-02-09 23:04:23 +0100

    Flask

--- Urodziny.py
+++ Urodziny.py
@@ -1,9 +1,48 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def jestem():
+    return "nie wiem o co tu chodzi!!!"
+
+slownik = {"Justyna": datetime.date(year=1987, month=6, day=17),
+           "Filip": datetime.date(year=1987, month=3, day=26),
+           "Stasio": datetime.date(year=2012, month=5, day=24),
+           "Niunia": datetime.date(year=2016, month=2, day=25)}
+
+def imie_w_bazie(name):
+    if name in slownik:
+        return ile_jeszcze(name)
+    print("Jakie jest Twoje imię?")
+    name = input()
+    print("Podaj rok urodzenia")
+    y = int(input())
+    print("Podaj miesiąc urodzenia")
+    m = int(input())
+    print("Podaj dzień urodzenia")
+    d = int(input())
+    slownik[name] = datetime.date(year = y, month = m, day = d)
+
+    return ile_jeszcze(name)
+
+@app.route('/name/<name>')
+def ile_jeszcze(name):
+    today = datetime.date.today()
+    bd = slownik[name]
+    rok = today.year
+    bd = bd.replace(year=rok)
+    if today > bd:
+        bd = bd.replace(year=rok+1)
+    ile = bd - today
+    return "Do Twoich urodzin zostało już tylko {} dni!!!".format(ile.days)
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+
+
+
